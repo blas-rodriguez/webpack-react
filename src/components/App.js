0194e5def09aa13f1f37bd9ddeb8e49fe3335ca7
blas-rodriguez <blasrodriguez934@gmail.com>
2022-01-29 15:57:16 -0300

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const App = () => `<h1>hola React1<h1/>`;
+
+export default App;
---
+++ src/components/App.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const App = () => "<h1>hola React<h1/>";
+
+export default App;
